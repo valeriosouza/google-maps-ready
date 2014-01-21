adb7048eb577652750b5676c4646defae40ef031
Google Maps plugin <Google Maps plugin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-21 16:33:16 +0000

    trunk folder
    git-svn-id: https://plugins.svn.wordpress.org/google-maps-ready/trunk@842514 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/icons/mod.php
+++
@@ -1,9 +0,0 @@
-<?php
-class  iconsGmp extends moduleGmp {
-    public function init(){
-        parent::init();
-        frameGmp::_()->addScript('iconOpts', $this->getModPath() .'js/iconOpts.js');
-    }
-   
-        
-}
